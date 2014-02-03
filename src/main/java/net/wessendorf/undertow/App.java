0d2f0d6ae12783ed9c41468b022592b315bcf900
Matthias Wessendorf <matzew@apache.org>
2014-02-03 09:36:25 +0100

    Test JAX-RS/Undertow server

---
+++ src/main/java/net/wessendorf/undertow/App.java
@@ -0,0 +1,45 @@
+package net.wessendorf.undertow;
+
+import io.undertow.Undertow;
+import io.undertow.server.handlers.resource.FileResourceManager;
+import net.wessendorf.undertow.jaxrs.TestApplication;
+import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import static io.undertow.Handlers.resource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main( String[] args ) {
+
+        UndertowJaxrsServer ut = new UndertowJaxrsServer();
+
+        TestApplication ta = new TestApplication();
+
+        ut.deploy(ta);
+
+        ut.start(
+                Undertow.builder()
+                        .addHttpListener(8080, "localhost")
+                        .setHandler(resource(new FileResourceManager(new File(System.getProperty("user.home")), 100))
+                                .setDirectoryListingEnabled(true))
+
+        );
+
+        try {
+            Thread.currentThread().join();
+        }
+        catch (InterruptedException e) {
+            logger.info("shutting down");
+            ut.stop();
+        }
+    }
+}
---
+++ src/test/java/net/wessendorf/undertow/AppTest.java
@@ -0,0 +1,5 @@
+package net.wessendorf.undertow;
+
+public class AppTest {
+
+}
